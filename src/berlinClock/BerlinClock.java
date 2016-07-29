ff5b9800d5902520fa13103b26ca113200491715
Eli Zukowski <eli.zukowski@gmail.com>
2016-07-29 12:46:21 -0400

    07/29/16 1245hrs: Added method stubs in BerlinClock

--- src/berlinClock/BerlinClock.java
+++ src/berlinClock/BerlinClock.java
@@ -2,6 +2,42 @@ package berlinClock;
 
 public class BerlinClock
 {
+
+	public String getSeconds(int seconds)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTopHours(int hours)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getBottomHours(int hours)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTopMinutes(int minutes)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getBottomMinutes(int minutes)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String[] convertToBerlinTime(String time)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
